5616e87bbaa41dff304981775f78561341938e30
Yun Hyeon Seung <hyun136000@gamil.com>
2026-06-23 19:10:46 +0900

    asd

--- account-book/src/main.jsx
+++ account-book/src/main.jsx
@@ -16,7 +16,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import store from "./store";
 import App from "./App.jsx";
 import "./index.css";
-
+console.log("API URL:", import.meta.env.VITE_API_BASE_URL);
 // React Query 클라이언트 설정
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -38,5 +38,5 @@ createRoot(document.getElementById("root")).render(
         </BrowserRouter>
       </QueryClientProvider>
     </Provider>
-  </StrictMode>
+  </StrictMode>,
 );
